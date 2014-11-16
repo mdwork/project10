5b1e215052bb83e71d5652505ed973a3d859fe10
mdwork <mazdze@gmail.com>
2014-11-16 18:05:42 +0200

    Zhe1ka

--- js/common_js.js
+++ js/common_js.js
@@ -21,4 +21,53 @@ $(document).ready(function(){
             curLabel.addClass('bg-label_js');
         }
     });
+
+    /*popup function*/
+    function popupWindow(targetClick, showCurrentForm) {
+        targetClick.on('click', function (e) {
+            e.preventDefault();
+
+            var bgPopup = $('#bg-popup'),
+                fotoPopup = $('#wrap-popup');
+
+            bgPopup.addClass('show_js');
+            fotoPopup.animate({'opacity': 1}, 500);
+            showCurrentForm.addClass('show_js');
+
+            bgPopup.height($(document).height());
+
+            var fotoInPopupW = fotoPopup.width(),
+                scrollTop = window.pageYOffset;
+
+            fotoPopup.css({
+                'top': scrollTop + 100,
+                'left': '50%',
+                'margin-left': - (fotoInPopupW / 2)
+            });
+
+            $('.btn-close, #bg-popup').on('click', function() {
+                bgPopup.removeClass('show_js');
+                showCurrentForm.removeClass('show_js');
+
+                $(showCurrentForm).find('.show_js').removeClass('show_js');
+
+                fotoPopup.css({
+                    'opacity' : 0,
+                    'top': 0,
+                    'left': 0,
+                    'margin-left': 0
+                });
+            });
+
+            $("#wrap-popup").click(function(e) {
+                e.stopPropagation();
+            });
+        });
+    }
+    /*end*/
+
+    var curLinkAuthorization = $('.box-authorization a'),
+        curBlockShow = $('.reg-popup');
+
+    popupWindow(curLinkAuthorization, curBlockShow);
 });
