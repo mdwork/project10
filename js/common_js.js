d2362673337095077b837715722d93ec1d630731
mdwork <mazdze@gmail.com>
2014-11-16 12:01:41 +0200

    MIB

--- js/common_js.js
+++ js/common_js.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
     $('.sidebar').height($('.wrapper').height());
+
+    $('input.check-filter').change('click', function(e){
+        var curLabel = $(e.target).parent();
+
+        if(curLabel.hasClass('bg-label_js')) {
+            curLabel.removeClass('bg-label_js');
+        }
+        else if (!curLabel.hasClass('bg-label_js')) {
+            curLabel.addClass('bg-label_js');
+        }
+    });
 });
