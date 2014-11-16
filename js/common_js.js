8f910f085c173e1eb6270e86c4dfe43195c6e8e8
mdwork <mazdze@gmail.com>
2014-11-16 16:41:39 +0200

    Zhe1ka

--- js/common_js.js
+++ js/common_js.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
-    $('input.check-filter').change('click', function(e){
+    $('.check-filter').change(function(e){
+        var curLabel = $(e.target).parent();
+
+        if(curLabel.hasClass('bg-label_js')) {
+            curLabel.removeClass('bg-label_js');
+        }
+        else if (!curLabel.hasClass('bg-label_js')) {
+            curLabel.addClass('bg-label_js');
+        }
+    });
+
+    $('.check-filter-radio').change(function(e){
+        var resetClassLabel = $(e.target).parent().parent().parent().find('label').removeClass('bg-label_js');
         var curLabel = $(e.target).parent();
 
         if(curLabel.hasClass('bg-label_js')) {
